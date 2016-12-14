2a6ec894a15f19df86d3557e08aaaf826c7f90b7
jsheperd <juhasz.janos.flutemaker@gmail.com>
2016-12-14 22:49:15 +0100

    More bones

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
-app.use(express.static(path.join(__dirname + '/../client/'))
+app.use(express.static(path.join(__dirname + '/../client/')))
 app.get('/', function(req, res) {
   res.send('Hello World')
 })
